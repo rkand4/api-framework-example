b9a6e7f8509530ae4b390826303d51512d6bab92
Owen Rubel <orubel@gmail.com>
2016-02-09 08:40:44 -0800

    adding hook views prior to changing to static HTML

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -8,13 +8,7 @@ String chainEntryPoint = "c${apiVersion}"
 String metricsEntryPoint = "m${apiVersion}"
 String domainEntryPoint = "d${apiVersion}"
 
-// Added by the Spring Security Core plugin:
-// grails.plugin.springsecurity.rememberMe.persistent = true
-// grails.plugin.springsecurity.rememberMe.persistentToken.domainClassName = 'net.nosegrind.apiframework.PersistentLogin'
 
-grails.plugin.springsecurity.adh.errorPage = null
-
-grails.plugin.springsecurity.providerNames = ['daoAuthenticationProvider', 'anonymousAuthenticationProvider', 'rememberMeAuthenticationProvider']
 
 // move to RequestMap once stabilized
 grails.plugin.springsecurity.securityConfigType = "InterceptUrlMap"
@@ -56,35 +50,38 @@ grails.plugin.springsecurity.rememberMe.alwaysRemember = true
 grails.plugin.springsecurity.rememberMe.cookieName = 'apiTest'
 grails.plugin.springsecurity.rememberMe.key = '_grails_'
 
+
 grails.plugin.springsecurity.logout.postOnly = false
 grails.plugin.springsecurity.ui.encodePassword = false
 grails.plugin.springsecurity.auth.forceHttps = false
-grails.plugin.springsecurity.auth.loginFormUrl = '/login/auth/'
-grails.plugin.springsecurity.auth.ajaxLoginFormUrl = '/login/authAjax/'
+//grails.plugin.springsecurity.auth.loginFormUrl = '/login/auth/'
+//grails.plugin.springsecurity.auth.ajaxLoginFormUrl = '/login/authAjax/'
 
-grails.plugin.springsecurity.failureHandler.defaultFailureUrl = '/'
-grails.plugin.springsecurity.failureHandler.ajaxAuthFailUrl = '/'
+grails.plugin.springsecurity.successHandler.defaultTargetUrl = '/login/ajaxSuccess'
+grails.plugin.springsecurity.failureHandler.defaultFailureUrl = '/login/ajaxDenied'
+grails.plugin.springsecurity.failureHandler.ajaxAuthFailUrl = '/login/ajaxDenied'
 
 grails.plugin.springsecurity.interceptUrlMap = [
-        "/${entryPoint}/**":'permitAll',
-        '/**':              'permitAll',
-        '/':                'permitAll',
-        '/error':           'permitAll',
-        '/index':           'permitAll',
-        '/index.gsp':       'permitAll',
-        '/shutdown':        'permitAll',
-        '/assets/**':       'permitAll',
-        '/error/**' :       'permitAll',
-        '/hook/**' :        'permitAll',
-        '/apidoc/**' :      'permitAll',
-        '/**/js/**':        'permitAll',
-        '/**/css/**':       'permitAll',
-        '/**/images/**':    'permitAll',
-        '/**/favicon.ico':  'permitAll',
-        '/login':           'permitAll',
-        '/login/**':        'permitAll',
-        '/logout':          'permitAll',
-        '/logout/**':       'permitAll'
+        "/${entryPoint}/**":['IS_AUTHENTICATED_ANONYMOUSLY'],
+        '/**':              ['IS_AUTHENTICATED_ANONYMOUSLY'],
+        '/':                ['IS_AUTHENTICATED_ANONYMOUSLY'],
+        '/error':           ['IS_AUTHENTICATED_ANONYMOUSLY'],
+        '/index':           ['IS_AUTHENTICATED_ANONYMOUSLY'],
+        '/index.gsp':       ['IS_AUTHENTICATED_ANONYMOUSLY'],
+        '/shutdown':        ['IS_AUTHENTICATED_ANONYMOUSLY'],
+        '/assets/**':       ['IS_AUTHENTICATED_ANONYMOUSLY'],
+        '/error/**' :       ['IS_AUTHENTICATED_ANONYMOUSLY'],
+        '/apidoc/**' :      ['IS_AUTHENTICATED_ANONYMOUSLY'],
+        '/**/js/**':        ['IS_AUTHENTICATED_ANONYMOUSLY'],
+        '/**/css/**':       ['IS_AUTHENTICATED_ANONYMOUSLY'],
+        '/**/images/**':    ['IS_AUTHENTICATED_ANONYMOUSLY'],
+        '/**/favicon.ico':  ['IS_AUTHENTICATED_ANONYMOUSLY'],
+        '/auth':            ['IS_AUTHENTICATED_ANONYMOUSLY'],
+        '/auth/**':         ['IS_AUTHENTICATED_ANONYMOUSLY'],
+        '/login':           ["permitAll"],
+        '/login/**':        ["permitAll"],
+        '/logout':          ["permitAll"],
+        '/logout/**':       ["permitAll"]
 ]
 
 
